8ab08340440a92939d0d55d62db70750ebe525e7
Hager Mahmoud Ashry <69155232+hajer772@users.noreply.github.com>
2026-02-13 13:51:43 +0000

    CSS Added

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 function sayHello() {
     const name = document.getElementById("nameInput").value;
 
+    if (name!==""){
     const greet = Module.cwrap('greet', 'string', ['string']);
     const message = greet(name);
 
     document.getElementById("result").innerText = message;
+    }
+
 }
